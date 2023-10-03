5ef4cbdde9a7d397281a26dca05279fea0e6dbee
smyshlenyj <103288354+smyshlenyj@users.noreply.github.com>
2023-10-03 11:52:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template <typename T> concept ComplexConcept = requires (const T v)
+{
+    { v.hash() } -> std::convertible_to<long>;
+    { v.toString() } -> std::same_as<std::string>;
+    { std::has_virtual_destructor<T>::value != true };
+};
+
+void printSmth(ComplexConcept auto &t)
+{
+    std::cout << t.toString() << std::endl;
+}
+
+struct maybeString
+{
+    std::string str = "Maybe yes, maybe no";
+    int number = 777;
+    std::string toString() const { return str; }
+    int hash() const { return number; }
+};
+
+int main()
+{
+    maybeString test;
+    printSmth(test);
+
+    return 0;
+}
